eaa553d125b40d1d9122819f136c26c99c081be5
magic-als <acheamponggodfred98@gmail.com>
2023-03-16 18:36:00 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,13 +2,12 @@
 
 
 /**
- *
  * main - prints "Programming is like building a multilingual puzzle
- * followed by a new line
+ * followed by a new line.
  * Return: Always 0
  */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle\n");
 	return (0);
 }
