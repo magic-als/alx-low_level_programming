dc535be22679c28225091e408cf3de588e9c60f3
magic-als <acheamponggodfred98@gmail.com>
2023-03-16 18:50:22 -0700

    error main

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,11 @@
-#include <stdio.h>
-
+#include <stdio.hi>
 
 /**
- * main - prints "Programming is like building a multilingual puzzle
- * followed by a new line.
+ * main - main blog
  * Return: Always 0
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
