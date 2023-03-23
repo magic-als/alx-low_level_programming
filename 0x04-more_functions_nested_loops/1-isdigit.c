bf2ba80cb60c9c262d32296973f7dbe76bdb7300
magic-als <acheamponggodfred98@gmail.com>
2023-03-23 11:58:11 -0700

    1-isdigit

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,12 +1,13 @@
 #inlude "main.h"
 
 /**
- * _isdigit - checks for a digit 0 through 9
- * @c: The character to be checked
- * Return: 1 if character is a digit or 0 for anything else
+ * _isdigit - checks if a character is a digit
+ * @c: The number to be checked
+ * Return: 1 for a character that will be a digit or 0 for otherwise
  */
 
 int _isdigit(int c)
+
 {
 
 	if (c >= 48 && c <= 57)
