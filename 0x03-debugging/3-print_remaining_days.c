fbe105a5630e0199bf47f2c0023aef2e700fe4b0
magic-als <acheamponggodfred98@gmail.com>
2023-03-23 00:57:33 -0700

    print remaining days

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -10,7 +10,7 @@
  * Return: void
  */
 
-void print_remainin_days(int month, int day, int year)
+void print_remaining_days(int month, int day, int year)
 
 {
 
@@ -27,7 +27,7 @@ void print_remainin_days(int month, int day, int year)
 	{
 	if (month == 2 && day == 60)
 	{
-	printf("Invalid date: %02d/%o2d/%04d\n", month, day - 31, year);
+	printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
 	}
 	else
 	{
