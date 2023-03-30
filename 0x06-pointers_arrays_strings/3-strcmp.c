0c0fca3fb3c07011841de4ba93ce194c1d8b577a
magic-als <acheamponggodfred98@gmail.com>
2023-03-29 18:07:51 -0700

    strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -4,11 +4,9 @@
  * _strcmp - Compare pointers to two strings
  * @s1: A pointer to the first string to be compared
  * @s2: A pointer to the second string to be compared
- * Return: If str1 < str2, the negative difference
- * of the first unmatched characters
+ * Return: If str1 < str2, the first negative difference
  * If str1 == str2, 0
- * If str1 > str2, the positive difference
- * of the first unmatched characters
+ * If str1 > str2, the first positive difference
  *
  */
 
