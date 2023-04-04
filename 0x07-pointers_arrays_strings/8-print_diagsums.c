2bd41384ca5094be14b8c9abaab4c6d7411e40f7
magic-als <acheamponggodfred98@gmail.com>
2023-04-03 18:45:58 -0700

    print giagsums

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -16,7 +16,7 @@ void print_diagsums(int *a, int size)
 
 	for (i = 0; i <= (size * size); i = i + size + 1)
 	{
-	sum = sum1 + a[i];
+	sum1 = sum1 + a[i];
 	}
 	for (n = size - 1; n <= (size * size) - size; n = n + size - 1)
 	{
