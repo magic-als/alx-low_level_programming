c89a74a1af5dadc5ec3652a2c4bc03b25518a157
magic-als <acheamponggodfred98@gmail.com>
2023-04-08 15:57:31 -0700

    what is my name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Prints it's name, followed by a new line
+ * @argc: Argument counter
+ * @argv: Argument vector
+ * Return: Always 0 (Success)
+ */
+
+int main(int argc, char *argv[])
+
+{
+	(void) argc;
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
---
+++ 0x0A-argc_argv/_putchar.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <time.h>
+
+/**
+ * main - Writes the character c to stdout
+ * @c: The character to print
+ * Return: On success 1
+ * On error, -1 is returned, and error is set appropriately
+ */
+
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
