a99182b4e7cb42a4c0a50440e4f8790c99a1ed22
magic-als <acheamponggodfred98@gmail.com>
2023-04-17 17:37:50 -0700

    main

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - Prints the name of the file it was compiled from.
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	printf("Compiled from %s\n", __FILE__);
+
+	return (0);
+}
